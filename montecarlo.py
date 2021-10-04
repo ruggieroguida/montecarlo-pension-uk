276a05fda18b9776a0cf4cf8890ba878a163e156
Ruggiero Guida <ruggiero.guida@gmail.com>
2021-10-04 22:05:37 +0100

    All inputs in a separate file

--- montecarlo.py
+++ montecarlo.py
@@ -12,41 +12,48 @@ with open('inputs.json') as f:
     inputs = json.load(f)
 
 # Personal data
-starting_capital = inputs['starting_capital']    # Starting capital
-retirement_age = inputs['retirement_age']        # Retirement age
-samples = inputs['samples']                      # Number of samples for the Montecarlo simulation
-dob = dt.datetime.strptime(inputs['dob'], '%Y-%m-%d')
+starting_capital = inputs["personal"]['starting_capital']    # Starting capital
+retirement_age = inputs["personal"]['retirement_age']        # Retirement age
+# Number of samples for the Montecarlo simulation
+samples = inputs["personal"]['samples']
+dob = dt.datetime.strptime(inputs["personal"]['dob'], '%Y-%m-%d')
 age = age(dob)
-max_age = inputs['max_age']             # The age you expect to die
+max_age = inputs["personal"]['max_age']             # The age you expect to die
 
 # Allocation
-current_stocks = 0.72                   # Starting stocks percentage in portfolio
-current_bonds = (1 - current_stocks)    # Starting bonds percentage in portfolio
-stocks_to_bonds = 0.01                  # Because you are not getting any younger, each year the portfolio is rebalanced towards bonds by this percentage
-min_stocks = 0.4                        # Minimum percentage of stocks in portfolio
-max_bonds  = (1.0 - min_stocks)         # Calculated maximum percentage of bonds
+current_stocks = inputs["allocation"]["current_stocks"]     # Starting stocks percentage in portfolio
+current_bonds = (1 - current_stocks)                        # Starting bonds percentage in portfolio
+# Because you are not getting any younger, each year the portfolio is rebalanced towards bonds by this percentage
+stocks_to_bonds = inputs["allocation"]["stocks_to_bonds"]
+min_stocks = inputs["allocation"]["min_stocks"]             # Minimum percentage of stocks in portfolio
+max_bonds = (1.0 - min_stocks)                              # Calculated maximum percentage of bonds
 
 # Returns and volatility
-mu_stocks = 0.04 # Stocks return after inflation
-mu_bonds = 0.005 # Bonds return after inflation
-vol_stocks = 0.25 # Stocks volatility (standard deviation)
-vol_bonds = 0.05 # Bonds volatility (standard deviation)
+mu_stocks = inputs["returns"]["mu_stocks"]      # Stocks return after inflation
+mu_bonds = inputs["returns"]["mu_bonds"]        # Bonds return after inflation
+vol_stocks = inputs["returns"]["vol_stocks"]    # Stocks volatility (standard deviation)
+vol_bonds = inputs["returns"]["vol_bonds"]      # Bonds volatility (standard deviation)
 
 
 # Contribution before retirement
-personal_contribution = 2000.0    # Monthly personal contribution without government topup (Net). This does not include work match
-work_contribution     = 263.0 * 2 # Monthly work contribution without government topup. Multiply by 2 if you match the company contribution
-annual_contribution = 12.0 * personal_contribution + 12 * work_contribution 
-contribution_growth = 0.02 # Because of payrise for example
-gov_topup = 0.25         # Higher tax rate. This is valid only in UK and as of 2021
-contribution_during_retirment = 0.0     # If for example you have other sources of income
+# Monthly personal contribution without government topup (Net). This does not include work match
+personal_contribution = inputs["contributions"]["personal"]
+# Monthly work contribution without government topup. Multiply by 2 if you match the company contribution
+employee_contribution = inputs["contributions"]["employee"]
+employer_contribution = inputs["contributions"]["employer"]
+work_contribution = employee_contribution + employer_contribution
+annual_contribution = 12.0 * personal_contribution + 12 * work_contribution
+contribution_growth = inputs["contributions"]["growth"]  # Because of payrise for example
+gov_topup = inputs["contributions"]["gov_topup"]         # Higher tax rate. This is valid only in UK and as of 2021
+# If for example you have other sources of income
+contribution_during_retirment = inputs["contributions"]["retirement"]
 
 
-# Expenses During retirement
-annual_expenses = 12 * 2000.0           # Net expenses once you retire.
-tax_free_allowance = 0.25               # Valid in UK
-state_pension = 9100.0                  # After 66 as of 2021
-state_pension_age = 66                  # As of 2021    
+# Expenses during retirement. During working years expenses do not count
+annual_expenses = 12 * inputs["retirement"]["monthly_expenses"]          # Net expenses once you retire.
+tax_free_allowance = inputs["retirement"]["tax_free_allowance"]          # Valid in UK
+state_pension = inputs["retirement"]["state_pension"]                    # After 66 as of 2021
+state_pension_age = inputs["retirement"]["state_pension_age"]            # As of 2021
 
 
 # Taxes calculation
@@ -54,18 +61,24 @@ state_pension_age = 66                  # As of 2021
 # more to cover expenses
 sustainable_withdrawal_rate = False     # Need to review
 swr_percentage = 0.04                   # Need to review
-bands = inputs['tax_bands'] 
-rates = inputs['tax_rates']
+bands = inputs["tax"]['tax_bands']
+rates = inputs["tax"]['tax_rates']
+
+
+
 
 # You pay taxes only on 75% of what you take out of your pension.
 # This assumes that you do not take a lump sum
 taxable_annual_expenses = (1 - tax_free_allowance) * annual_expenses
-annual_withdrawal = net_to_gross(taxable_annual_expenses, bands, rates) + tax_free_allowance * annual_expenses # Total gross withdrawal
- 
+annual_withdrawal = net_to_gross(taxable_annual_expenses, bands, rates) + \
+    tax_free_allowance * annual_expenses  # Total gross withdrawal
+
 
 living_years = max_age - age                    # Number of years left
-working_years = retirement_age - age            # Number of working years based on your expected retiremen age
-state_pension_years = state_pension_age - age   # Number of years before state pension
+# Number of working years based on your expected retiremen age
+working_years = retirement_age - age
+state_pension_years = state_pension_age - \
+    age   # Number of years before state pension
 print("age:                     " + str(age))
 print("Retirement age:          " + str(retirement_age))
 print("Annual withdrawal        " + str(annual_withdrawal))
@@ -74,9 +87,6 @@ print("No state pension years:  " + str(working_years))
 print("Years left:              " + str(living_years))
 
 
-#############################################
-#      Calculations. No more inputs         #
-#############################################
 final_value = []
 value_55 = []
 value_retirement = []
@@ -87,79 +97,90 @@ min_annual_withdrawals = []
 
 print('Samples: ' + str(samples))
 for i in range(samples):
-    if i%(samples / 10) == 0:
+    if i % (samples / 10) == 0:
         iteration_text = str(i) + '.. '
         sys.stdout.write(iteration_text)
-        
+
     annual_investment = annual_contribution * (1.0 + gov_topup)
     stocks = current_stocks
-    bonds  = current_bonds
+    bonds = current_bonds
 
     # Create list of daily returns using random normal distribution
-    annual_returns_stocks = np.random.normal(mu_stocks, vol_stocks, living_years)
-    annual_returns_bonds  = np.random.normal(mu_bonds, vol_bonds, living_years)
-    
+    annual_returns_stocks = np.random.normal(
+        mu_stocks, vol_stocks, living_years)
+    annual_returns_bonds = np.random.normal(mu_bonds, vol_bonds, living_years)
+
     # Set starting capital and create annual capital series generated by above random daily returns
     annual_values = [starting_capital]
     annual_withdrawals = []
-    
+
     # Year is a counter from now
     for year, (annual_return_stocks, annual_return_bonds) in enumerate(zip(annual_returns_stocks, annual_returns_bonds)):
 
         # Assume we invest at the end of the year - Conservative assumption I think
-        if year <= working_years: # Before retirement
-            starting_annual_value = annual_values[-1] # From previous year
+        if year <= working_years:  # Before retirement
+            starting_annual_value = annual_values[-1]  # From previous year
             final_annual_value = stocks * starting_annual_value * (annual_return_stocks + 1.0) + \
-                                 bonds  * starting_annual_value * (annual_return_bonds   + 1.0)     # Change of previous year capital        
-            annual_values.append(final_annual_value + annual_investment)                            # Additional investemnt at the end of year 
-        else: # After retirement
+                bonds * starting_annual_value * \
+                (annual_return_bonds + 1.0)     # Change of previous year capital
+            # Additional investemnt at the end of year
+            annual_values.append(final_annual_value + annual_investment)
+        else:  # After retirement
             # Assume we withdraw at the beginning of the year - Conservative assumption
-            if sustainable_withdrawal_rate: # To be reviewed
-                if swr_percentage * annual_values[-1] > annual_withdrawal:    
+            if sustainable_withdrawal_rate:  # To be reviewed
+                if swr_percentage * annual_values[-1] > annual_withdrawal:
                     actual_annual_withdrawal = annual_withdrawal
                 else:
-                    actual_annual_withdrawal = swr_percentage * annual_values[-1]
+                    actual_annual_withdrawal = swr_percentage * \
+                        annual_values[-1]
             else:
                 if year <= state_pension_years:
-                    actual_annual_withdrawal = annual_withdrawal                  # I need to withdraw all expected expesnes
+                    # I need to withdraw all expected expesnes
+                    actual_annual_withdrawal = annual_withdrawal
                 else:
-                    actual_annual_withdrawal = annual_withdrawal - state_pension  # Little help from state pension
+                    actual_annual_withdrawal = annual_withdrawal - \
+                        state_pension  # Little help from state pension
             annual_withdrawals.append(actual_annual_withdrawal)
-            
-            starting_annual_value = annual_values[-1] - actual_annual_withdrawal
-            final_annual_value = stocks * starting_annual_value * (annual_return_stocks + 1.0) + \
-                                 bonds  * starting_annual_value * (annual_return_bonds  + 1.0)
-            annual_values.append(final_annual_value)
-        
-        if year <= working_years:                               # Before retirement
-            annual_investment *= (1.0 + contribution_growth)    # Keep investing until retiremnt
-        else:                                                   # After retirement - No growth
-            annual_investment = contribution_during_retirment   # Assume it will not increase. Safely I assume 0.0
-            
 
-        stocks = max(min_stocks, stocks - stocks_to_bonds) # Limit stocks contribution to min_stocks% 
-        bonds  = min(max_bonds, bonds  + stocks_to_bonds) # Limit bonds contribution to max_bonds%
-    
-    min_annual_withdrawal = min(annual_withdrawals) # This is only valid if sustainable_withdrawal_rate == True
-    
-    #Generate Plots - price series and histogram of daily returns
+            starting_annual_value = annual_values[-1] - \
+                actual_annual_withdrawal
+            final_annual_value = stocks * starting_annual_value * (annual_return_stocks + 1.0) + \
+                bonds * starting_annual_value * (annual_return_bonds + 1.0)
+            annual_values.append(final_annual_value)
+
+        if year <= working_years:                               # Before retirement
+            # Keep investing until retiremnt
+            annual_investment *= (1.0 + contribution_growth)
+        else:                                                   # After retirement - No growth
+            # Assume it will not increase. Safely I assume 0.0
+            annual_investment = contribution_during_retirment
+
+        # Limit stocks contribution to min_stocks%
+        stocks = max(min_stocks, stocks - stocks_to_bonds)
+        # Limit bonds contribution to max_bonds%
+        bonds = min(max_bonds, bonds + stocks_to_bonds)
+
+    # This is only valid if sustainable_withdrawal_rate == True
+    min_annual_withdrawal = min(annual_withdrawals)
+
+    # Generate Plots - price series and histogram of daily returns
     # plt.plot(annual_values)
 
     # Let's create some statistics at various ages
-    year_55 = 55 - age # Minimum retirment age in UK as of 2020
+    year_55 = 55 - age  # Minimum retirment age in UK as of 2020
     value_55.append(annual_values[year_55])
-    
+
     year_retirement = retirement_age - age
     value_retirement.append(annual_values[year_retirement])
-    
+
     year_85 = 85 - age
     value_85.append(annual_values[year_85])
 
     year_100 = 100 - age
     value_100.append(annual_values[year_100])
-    
+
     final_value.append(annual_values[-1])
-    
+
     zero_capital_year.append(find_first_negative(annual_values) + age)
 
     min_annual_withdrawals.append(min_annual_withdrawal)
